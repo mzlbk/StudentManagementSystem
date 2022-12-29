b2c2d669f9eb2b32f16f7e1ac8b94f11aa15f1f0
mzlbk <muzlufl@gmail.com>
2022-12-30 00:06:24 +0300

    Update LicenceCourses.java

--- src/LicenceCourses.java
+++ src/LicenceCourses.java
@@ -1,4 +1,7 @@
 public class LicenceCourses extends Courses
 {
-
+    public LicenceCourses(String courseName, int courseCode, int courseCredit)
+    {
+        super(courseName, courseCode, courseCredit);
+    }
 }
