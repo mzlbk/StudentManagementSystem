96c9563ca78cef6ffefee40ce8ac7f91b144c763
mzlbk <muzlufl@gmail.com>
2022-12-30 00:06:17 +0300

    Update LecturerRooms.java

--- src/LecturerRooms.java
+++ src/LecturerRooms.java
@@ -1,4 +1,19 @@
 public class LecturerRooms extends Areas
 {
-
+    private int floor;
+    public int getFloor() {
+        return floor;
+    }
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+    public LecturerRooms()
+    {
+        this.floor = 0;
+    }
+    public LecturerRooms(String areasName, int floor)
+    {
+        super(areasName);
+        this.floor = floor;
+    }
 }
