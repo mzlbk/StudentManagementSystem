6b6791fc70d42d48fb5b1925f28064b89da4a108
mzlbk <muzlufl@gmail.com>
2022-12-30 00:06:11 +0300

    Update ClassroomNo.java

--- src/ClassroomNo.java
+++ src/ClassroomNo.java
@@ -1,4 +1,23 @@
 public class ClassroomNo extends Areas
 {
+    private int classroomCode;
+    public int getClassroomName()
+    {
+        return classroomCode;
+    }
+    public void setClassroomName(int classroomName)
+    {
+        this.classroomCode = classroomName;
+    }
 
+
+    public ClassroomNo()
+    {
+        this.classroomCode = 0;
+    }
+    public ClassroomNo(String areasName, int classroomCode)
+    {
+        super(areasName);
+        this.classroomCode = classroomCode;
+    }
 }
